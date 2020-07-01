c7caf791e8ace5b8ada2aa8978522ae5b4de259e
Holden Parker <holdenprkr@gmail.com>
2020-06-30 19:18:59 -0500

    making progress

--- app.js
+++ app.js
@@ -9,16 +9,51 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
+document.querySelector(".dice").style.display = "none";
 
-document.querySelector("#current-" + activePlayer).innerHTML = '<em>' + dice + '</em>'
+document.getElementById('score-0').textContent = 0
+document.getElementById('score-1').textContent = 0
+document.getElementById('current-0').textContent = 0
+document.getElementById('current-1').textContent = 0
+var diceDOM = document.querySelector(".dice");
 
-var x = document.querySelector('score-0').textContent
-console.log()
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + dice + ".png";
+
+  if (dice !== 1) {
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    nextPlayer();
+
+    diceDOM.src = "dice-1.png";
+  }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    scores[activePlayer] += roundScore;
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    nextPlayer();
+
+    diceDOM.style.display = 'none';
+})
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0
+
+    document.getElementById('current-0').textContent = '0'
+    document.getElementById('current-1').textContent = '0'
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+}
